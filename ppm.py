ca1331fda465aab8223b4b69b9e76aacb88c51e8
John Hauck <JohnMichaelHauck@gmail.com>
2024-07-26 10:44:50 -0400

    Make the program a bit more mature

--- ppm.py
+++ ppm.py
@@ -27,40 +27,66 @@ class CompanyConstants:
         # the percentage of the selling price that is allocated to cover selling, general, and administrative expenses
         self.sga_percentage = 0.30
 
-class ProductVariables:
+class ProductVariablesRanges:
     def __init__(self):
         # the number of people (full time equivalents) on the development team
-        self.development_ftes = np.random.triangular(4, 5, 6)
+        self.development_ftes = [4, 5, 6]
         
-        # the number of years remaining to develop the product
-        self.remaining_development_years = round(np.random.triangular(3, 4, 5))
+        # the number of remaining years to develop the product
+        self.remaining_development_years = [3, 4, 5]
         
         # the number of remaining years after development that the product will be sold
-        self.remaining_delay_years = round(np.random.triangular(0, 1, 2))
+        self.remaining_delay_years = [0, 1, 2]
         
         # the number of people that need to keep working on the product after development
-        self.maintenance_ftes = np.random.triangular(0, 0.5, 1)
+        self.maintenance_ftes = [0, 0.5, 1]
         
         # the cost to manufacture one unit of the product (in present dollars)
-        self.unit_cost_pv = np.random.triangular(8000, 10000, 12000)
+        self.unit_cost_pv = [8000, 10000, 12000]
         
-        # the selling price of the product (in present dollars)
-        self.unit_price_pv = np.random.triangular(self.unit_cost_pv * 1.9, self.unit_cost_pv * 2, self.unit_cost_pv * 2.1)
-        
-        # the number of units that are expected to be sold per year
-        self.yearly_unit_sales = 100
-        
-        # the number of years that the product is expected to be sold
-        self.remaining_years_of_sales = 10
+        # the number of remaining years that the product is expected to be sold
+        self.remaining_sales_years = [8, 10, 12]
+
+        # the selling price cost factors to compute the price of the product (in present dollars)
+        self.unit_price_cost_factor = [1.9, 2.0, 2.1]
 
         # the number of units that are expected to be sold per year
-        price_range = np.array([16000, 24000])
-        sales_range = np.array([120, 80])
-        exact_sales = np.interp(20000, price_range, sales_range)
-        self.yearly_unit_sales = np.random.triangular(exact_sales * 0.9, exact_sales, exact_sales * 1.1)
+        self.yearly_unit_sales_lowest_price = [110, 120, 130]
+        self.yearly_unit_sales_highest_price = [70, 80, 90]
 
-        # the number of years that the product is expected to be sold
-        self.remaining_years_of_sales = round(np.random.triangular(8, 10, 12))
+    def lowest_price(self):
+        return self.unit_cost_pv[0] * self.unit_price_cost_factor[0]
+    
+    def highest_price(self):
+        return self.unit_cost_pv[2] * self.unit_price_cost_factor[2]
+
+def gen_t(a):
+    return np.random.triangular(a[0], a[1], a[2])
+
+class ProductVariablesSnapshot:
+    def __init__(self, product_variables_ranges):
+        
+        # convert ranges to actual values using a triangular distribution
+        self.development_ftes = gen_t(product_variables_ranges.development_ftes)
+        self.remaining_development_years = gen_t(product_variables_ranges.remaining_development_years)
+        self.remaining_delay_years = gen_t(product_variables_ranges.remaining_delay_years)
+        self.maintenance_ftes = gen_t(product_variables_ranges.maintenance_ftes)
+        self.unit_cost_pv = gen_t(product_variables_ranges.unit_cost_pv)
+        self.remaining_sales_years = gen_t(product_variables_ranges.remaining_sales_years)
+        self.unit_price_cost_factor = gen_t(product_variables_ranges.unit_price_cost_factor)
+        
+        # compute the unit price
+        self.unit_price_pv = self.unit_cost_pv * self.unit_price_cost_factor
+
+        # interpoloate the yearly unit sales range as a function of the unit price
+        price_range = np.array([product_variables_ranges.lowest_price(), product_variables_ranges.highest_price()])
+        yearly_unit_sales_range = [0, 0, 0]
+        for i in range(3):
+            sales_range_i = np.array([product_variables_ranges.yearly_unit_sales_lowest_price[i], product_variables_ranges.yearly_unit_sales_highest_price[i]])
+            yearly_unit_sales_range[i] = np.interp(self.unit_price_pv, price_range, sales_range_i)
+        
+        # convert range to actual value using a triangular distribution
+        self.yearly_unit_sales = gen_t(yearly_unit_sales_range)
 
 class NpvCalculationResult:
     def __init__(self):
@@ -72,7 +98,7 @@ class NpvCalculationResult:
     def npv(self):
         return self.future_sales - self.future_cost_of_goods - self.future_sga - self.future_development_cost
 
-def calculate_npv(product_variables, company_constants):
+def calculate_npv(product_variables_snapshot, company_constants):
 
     # this is what we will calculate and return 
     npv_calculation_result = NpvCalculationResult()
@@ -81,49 +107,49 @@ def calculate_npv(product_variables, company_constants):
     year = 0
 
     # let's compute the remaining product development costs, one year at a time
-    for _ in range(product_variables.remaining_development_years):
+    for _ in range(round(product_variables_snapshot.remaining_development_years)):
 
         # compute the future value of the cost of development for this year
         development_cost_fte_fv = fv(company_constants.development_fte_cost_pv, company_constants.development_cost_trend, year)
 
         # add the cost of development
-        npv_calculation_result.future_development_cost += pv(development_cost_fte_fv * product_variables.development_ftes, company_constants.market_return, year)
+        npv_calculation_result.future_development_cost += pv(development_cost_fte_fv * product_variables_snapshot.development_ftes, company_constants.market_return, year)
 
         # move to the next year
         year += 1
 
     # let's compute the cost of delay
-    for _ in range(product_variables.remaining_delay_years):
+    for _ in range(round(product_variables_snapshot.remaining_delay_years)):
 
         # compute the future value of the cost of development for this year
         development_cost_fte_fv = fv(company_constants.development_fte_cost_pv, company_constants.development_cost_trend, year)
 
         # add the cost of development
-        npv_calculation_result.future_development_cost += pv(development_cost_fte_fv * product_variables.maintenance_ftes, company_constants.market_return, year)
+        npv_calculation_result.future_development_cost += pv(development_cost_fte_fv * product_variables_snapshot.maintenance_ftes, company_constants.market_return, year)
 
         # move to the next year
         year += 1
 
 
     # let's compute and add in the sales
-    for j in range(product_variables.remaining_years_of_sales):
+    for j in range(round(product_variables_snapshot.remaining_sales_years)):
         
         # compute the future value of the selling price, unit cost, and cost of development for this year
-        list_price_fv = fv(product_variables.unit_price_pv, company_constants.unit_price_trend, year)
-        unit_cost_fv = fv(product_variables.unit_cost_pv, company_constants.unit_cost_trend, year)
+        unit_price_fv = fv(product_variables_snapshot.unit_price_pv, company_constants.unit_price_trend, year)
+        unit_cost_fv = fv(product_variables_snapshot.unit_cost_pv, company_constants.unit_cost_trend, year)
         development_cost_fte_fv = fv(company_constants.development_fte_cost_pv, company_constants.development_cost_trend, year)
         
         # compute the selling, general, and administrative expenses per unit
-        sga_fv = list_price_fv * company_constants.sga_percentage
+        sga_fv = unit_price_fv * company_constants.sga_percentage
 
         # compute the future value of this year
         development_cost_fte_fv = fv(company_constants.development_fte_cost_pv, company_constants.development_cost_trend, year)
 
         # add the cost of development, sales, cost of goods, and sga
-        npv_calculation_result.future_development_cost += pv(development_cost_fte_fv * product_variables.maintenance_ftes, company_constants.market_return, year)
-        npv_calculation_result.future_sales += pv(list_price_fv * product_variables.yearly_unit_sales, company_constants.market_return, year)
-        npv_calculation_result.future_cost_of_goods += pv(unit_cost_fv * product_variables.yearly_unit_sales, company_constants.market_return, year)
-        npv_calculation_result.future_sga += pv(sga_fv * product_variables.yearly_unit_sales, company_constants.market_return, year)
+        npv_calculation_result.future_development_cost += pv(development_cost_fte_fv * product_variables_snapshot.maintenance_ftes, company_constants.market_return, year)
+        npv_calculation_result.future_sales += pv(unit_price_fv * product_variables_snapshot.yearly_unit_sales, company_constants.market_return, year)
+        npv_calculation_result.future_cost_of_goods += pv(unit_cost_fv * product_variables_snapshot.yearly_unit_sales, company_constants.market_return, year)
+        npv_calculation_result.future_sga += pv(sga_fv * product_variables_snapshot.yearly_unit_sales, company_constants.market_return, year)
 
         # move to the next year
         year += 1
@@ -131,15 +157,16 @@ def calculate_npv(product_variables, company_constants):
     return npv_calculation_result
 
 company_constants = CompanyConstants()
+product_variables_ranges = ProductVariablesRanges()
 
-def simulate_npv():
-    product_variables = ProductVariables()
-    npv_calculation_result = calculate_npv(product_variables, company_constants)
+def simulate_one_npv():
+    product_variables_snapshot = ProductVariablesSnapshot(product_variables_ranges)
+    npv_calculation_result = calculate_npv(product_variables_snapshot, company_constants)
     return npv_calculation_result
 
 npvs = []
 for i in range(10000):
-    npv_calculation_result = simulate_npv()
+    npv_calculation_result = simulate_one_npv()
     npvs.append(npv_calculation_result.npv())
 plt.hist(npvs, bins=30)
 plt.xlabel('NPV')
